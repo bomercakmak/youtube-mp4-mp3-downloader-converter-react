eadf11254d82b4b57efc632ae4d85550197c0207
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-16 01:26:59 +0100

    I added two new state and new DownloadButton component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [videoUrl, setvideoUrl] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    const id = videoUrl.replace("https://www.youtube.com/watch?v=", "");
+    setId(id);
+  }, [videoUrl]);
+
   return (
     <div className="App">
       <div className="appNavbar">
@@ -8,8 +17,13 @@ function App() {
         <p>Welcome to my app!</p>
       </div>
       <div className="appLinkBox">
-        <form>
-          <input type="text" placeholder="Put your YouTube video link..."/>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            onChange={(e) => setvideoUrl(e.target.value)}
+            value={videoUrl}
+            type="text"
+            placeholder="Put your YouTube video link..."
+          />
         </form>
       </div>
     </div>
---
+++ src/DownloadButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DownloadButton.css'
+
+function DownloadButton() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DownloadButton
